a8c3739399637c59c01f9ae217b7f3847933773a
Justin Hays <jhays478@gmail.com>
2020-08-28 01:07:23 +0000

    init - able to receive and send UDP packets

---
+++ EO-Network-Protocol/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace EO_Network_Protocol
+{
+    class Program
+    {
+        private const int listenPort = 11000;
+        
+        static void Main(string[] args)
+        {
+            StartListener();
+        }
+
+        private static void StartListener()
+        {
+            UdpClient listener = new UdpClient(listenPort);
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, listenPort);
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Waiting for broadcast");
+                    byte[] bytes = listener.Receive(ref groupEP);
+
+                    Console.WriteLine($"Received broadcast from {groupEP} :");
+                    Send(groupEP);
+                    Console.WriteLine($" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}");
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                listener.Close();
+            }
+        }
+
+        private static void Send(IPEndPoint endpoint)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            IPAddress broadcast = IPAddress.Parse("192.168.1.255"); // get ipaddress from endpoint and use that for the broadcast
+
+            byte[] sendbuf = Encoding.ASCII.GetBytes("got it!");
+            IPEndPoint ep = new IPEndPoint(broadcast, 8051);
+
+            s.SendTo(sendbuf, ep);
+
+            Console.WriteLine("Message sent to the broadcast address");
+        }
+    }
+}
